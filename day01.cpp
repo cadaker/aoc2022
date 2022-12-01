e3c01a9c7ddb61c1e1aa7eaaa0d347ee792502b5
Christian Adåker <cadaker@gmail.com>
2022-12-01 10:42:11 +0100

    Day 01 part 1

--- day01.cpp
+++ day01.cpp
@@ -1,4 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using input_type = std::vector<std::vector<long>>;
+
+static input_type read_input(std::istream& is) {
+    input_type input;
+    std::string line;
+    bool has_data = true;
+    std::vector<long> block;
+    while (std::getline(is, line)) {
+        if (!line.empty()) {
+            block.push_back(std::stol(line));
+        } else if (!block.empty()) {
+            input.push_back(std::move(block));
+            block = {};
+        }
+    }
+    if (!block.empty()) {
+        input.push_back(std::move(block));
+    }
+    return input;
+}
+
+long total_calories(std::vector<long> const& elf) {
+    return std::accumulate(elf.begin(), elf.end(), 0L, std::plus<>{});
+}
 
 int main() {
+    auto input = read_input(std::cin);
+
+    auto max_elf = std::max_element(input.begin(), input.end(), [](auto const& e0, auto const& e1) {
+        return total_calories(e0) < total_calories(e1);
+    });
+
+    if (max_elf != input.end()) {
+        std::cout << total_calories(*max_elf) << "\n";
+    }
+
     return 0;
 }
