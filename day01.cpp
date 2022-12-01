7bbad746ee380599adf15632c0c051ca1e0de1c0
Christian Adåker <cadaker@gmail.com>
2022-12-01 10:46:13 +0100

    Day 01 part 2

--- day01.cpp
+++ day01.cpp
@@ -39,5 +39,15 @@ int main() {
         std::cout << total_calories(*max_elf) << "\n";
     }
 
+    std::sort(input.begin(), input.end(), [](auto const& e0, auto const& e1) {
+        return total_calories(e0) > total_calories(e1);
+    });
+
+    long total = 0;
+    for (size_t i = 0; i < input.size() && i < 3; ++i) {
+        total += total_calories(input[i]);
+    }
+    std::cout << total << "\n";
+
     return 0;
 }
