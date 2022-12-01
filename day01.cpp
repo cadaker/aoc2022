ce3cc30d79a153996825ac53d07b7315fa3b0ce1
Christian Adåker <cadaker@gmail.com>
2022-12-01 10:55:47 +0100

    Day 01 tidying

--- day01.cpp
+++ day01.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-#include <algorithm>
+#include <iterator>
 
 using input_type = std::vector<std::vector<long>>;
 
 static input_type read_input(std::istream& is) {
     input_type input;
     std::string line;
-    bool has_data = true;
     std::vector<long> block;
     while (std::getline(is, line)) {
         if (!line.empty()) {
@@ -28,26 +27,25 @@ long total_calories(std::vector<long> const& elf) {
     return std::accumulate(elf.begin(), elf.end(), 0L, std::plus<>{});
 }
 
+template<std::forward_iterator Iterator>
+Iterator take_n(size_t n, Iterator begin, Iterator end) {
+    for (size_t i = 0; i < n && begin != end; ++i, ++begin) {
+    }
+    return begin;
+}
+
 int main() {
     auto input = read_input(std::cin);
-
-    auto max_elf = std::max_element(input.begin(), input.end(), [](auto const& e0, auto const& e1) {
-        return total_calories(e0) < total_calories(e1);
-    });
-
-    if (max_elf != input.end()) {
-        std::cout << total_calories(*max_elf) << "\n";
-    }
 
     std::sort(input.begin(), input.end(), [](auto const& e0, auto const& e1) {
         return total_calories(e0) > total_calories(e1);
     });
 
-    long total = 0;
-    for (size_t i = 0; i < input.size() && i < 3; ++i) {
-        total += total_calories(input[i]);
-    }
-    std::cout << total << "\n";
+    std::cout << (input.empty() ? 0 : total_calories(input.front())) << "\n";
+
+    std::cout << std::accumulate(input.begin(), take_n(3, input.begin(), input.end()), 0L, [](long acc, std::vector<long> const& xs) {
+        return acc + total_calories(xs);
+    }) << "\n";
 
     return 0;
 }
