624e240131c539b3ed3fcf913748fb458fb0f9ae
Christian Adåker <cadaker@gmail.com>
2022-12-03 08:19:27 +0100

    Day 03 part 1

---
+++ day03.cpp
@@ -0,0 +1,46 @@
+#include "algorithm.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+std::vector<std::string> parse_input(std::istream& is) {
+    std::string line;
+    std::vector<std::string> ret;
+    while (std::getline(is, line)) {
+        ret.push_back(line);
+    }
+    return ret;
+}
+
+int item_priority(char item) {
+    if ('a' <= item && item <= 'z') {
+        return item - 'a' + 1;
+    } else {
+        return item - 'A' + 27;
+    }
+}
+
+int backpack_priority(std::string const& backpack) {
+    std::vector<char> comp1(backpack.begin(), backpack.begin() + backpack.size()/2);
+    std::vector<char> comp2(backpack.begin() + backpack.size()/2, backpack.end());
+
+    std::sort(comp1.begin(), comp1.end());
+    std::sort(comp2.begin(), comp2.end());
+
+    std::vector<char> intersection;
+    std::set_intersection(comp1.begin(), comp1.end(), comp2.begin(), comp2.end(), std::back_inserter(intersection));
+
+    if (!intersection.empty()) {
+        return item_priority(intersection.front());
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    auto input = parse_input(std::cin);
+
+    std::cout << accumulate_map(input.begin(), input.end(), 0L, backpack_priority) << "\n";
+}
