282a5d3bb7b3f6ac0692ddfba2e8b752ba1d2355
Christian Adåker <cadaker@gmail.com>
2022-12-07 10:29:25 +0100

    Day 07 part 2

--- day07.cpp
+++ day07.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <regex>
 #include <unordered_map>
+#include <limits>
 
 struct dir_t {
     std::unordered_map<std::string, long> files;
@@ -40,25 +41,40 @@ std::unique_ptr<dir_t> parse_input(std::istream& is) {
     return root;
 }
 
-long find_small_dirs(dir_t const& dir, long& total, long limit) {
+template<class F>
+long visit_dirs(dir_t const& dir, F&& func) {
     long dir_size = 0;
     for (auto const& f : dir.files) {
         dir_size += f.second;
     }
     for (auto const& d : dir.subdirs) {
-        long subdir_size = find_small_dirs(*d.second, total, limit);
+        long subdir_size = visit_dirs(*d.second, func);
         dir_size += subdir_size;
     }
-    if (dir_size <= limit) {
-        total += dir_size;
-    }
+    func(dir_size);
     return dir_size;
 }
+
+constexpr long SMALL_DIR_LIMIT = 100000;
+constexpr long FILE_SYSTEM_SIZE = 70000000;
+constexpr long SPACE_NEEDED = 30000000;
 
 int main() {
     auto input = parse_input(std::cin);
 
     long small_dir_total = 0;
-    find_small_dirs(*input, small_dir_total, 100000);
+    long const total_size = visit_dirs(*input, [&](long dir_size) {
+        if (dir_size <= SMALL_DIR_LIMIT) {
+            small_dir_total += dir_size;
+        }
+    });
     std::cout << small_dir_total << "\n";
+
+    long best_dir_size = std::numeric_limits<long>::max();
+    visit_dirs(*input, [&](long dir_size) {
+        if (FILE_SYSTEM_SIZE - (total_size - dir_size) >= SPACE_NEEDED && dir_size < best_dir_size) {
+            best_dir_size = dir_size;
+        }
+    });
+    std::cout << best_dir_size << "\n";
 }
