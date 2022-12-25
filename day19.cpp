3e2e49c0958c809111078ca7a48337aa04e65503
Christian Adåker <cadaker@gmail.com>
2022-12-25 14:12:13 +0100

    Day 19 significantly optimize solution for part 1

--- day19.cpp
+++ day19.cpp
@@ -59,13 +59,13 @@ struct state_t {
     long geode_robots = 0;
 };
 
-state_t advance(state_t const& state) {
+state_t advance(state_t const& state, long time = 1) {
     state_t new_state = state;
-    --new_state.time_left;
-    new_state.ore += new_state.ore_robots;
-    new_state.clay += new_state.clay_robots;
-    new_state.obsidian += new_state.obsidian_robots;
-    new_state.geodes += new_state.geode_robots;
+    new_state.time_left -= time;
+    new_state.ore += new_state.ore_robots * time;
+    new_state.clay += new_state.clay_robots * time;
+    new_state.obsidian += new_state.obsidian_robots * time;
+    new_state.geodes += new_state.geode_robots * time;
     return new_state;
 }
 
@@ -99,6 +99,28 @@ state_t build_geode_robot(state_t const& state, blueprint const& bp) {
     return new_state;
 }
 
+long until(long owned, long needed, long per_turn) {
+    if (owned >= needed) {
+        return 0;
+    } else if (per_turn == 0) {
+        return 100000; // Big, but not big enough to risk overflows.
+    } else {
+        return ((needed - owned) + per_turn - 1) / per_turn;
+    }
+}
+
+long time_until_ore(state_t const& state, long ore) {
+    return until(state.ore, ore, state.ore_robots);
+}
+
+long time_until_clay(state_t const& state, long clay) {
+    return until(state.clay, clay, state.clay_robots);
+}
+
+long time_until_obsidian(state_t const& state, long obsidian) {
+    return until(state.obsidian, obsidian, state.obsidian_robots);
+}
+
 long search(blueprint const& bp) {
     std::vector<state_t> stack = {{24, 0, 0, 0, 0, 1, 0, 0, 0}};
     long most_geodes = 0;
@@ -114,26 +136,32 @@ long search(blueprint const& bp) {
             continue;
         }
 
-        if (state.ore >= bp.ore_robot_ore_cost && state.ore_robots < bp.max_ore_cost()) {
-            stack.push_back(build_ore_robot(advance(state), bp));
+        // Build geode bot
+        long const geode_time = std::max(
+                time_until_ore(state, bp.geode_robot_ore_cost),
+                time_until_obsidian(state, bp.geode_robot_obsidian_cost));
+        if (state.time_left > geode_time) {
+            stack.push_back(build_geode_robot(advance(state, geode_time + 1), bp));
         }
 
-        if (state.ore >= bp.clay_robot_ore_cost && state.clay_robots < bp.obsidian_robot_clay_cost) {
-            stack.push_back(build_clay_robot(advance(state), bp));
+        long const obsidian_time = std::max(
+                time_until_clay(state, bp.obsidian_robot_clay_cost),
+                time_until_ore(state, bp.obsidian_robot_ore_cost));
+        if (state.time_left > obsidian_time && state.obsidian_robots < bp.geode_robot_obsidian_cost) {
+            stack.push_back(build_obsidian_robot(advance(state, obsidian_time + 1), bp));
         }
 
-        if (state.ore >= bp.obsidian_robot_ore_cost &&
-            state.clay >= bp.obsidian_robot_clay_cost &&
-            state.obsidian_robots < bp.geode_robot_obsidian_cost) {
-            stack.push_back(build_obsidian_robot(advance(state), bp));
+        long const clay_time = time_until_ore(state, bp.clay_robot_ore_cost);
+        if (state.time_left > clay_time && state.clay_robots < bp.obsidian_robot_clay_cost) {
+            stack.push_back(build_clay_robot(advance(state, clay_time + 1), bp));
         }
 
-        if (state.ore >= bp.geode_robot_ore_cost && state.obsidian >= bp.geode_robot_obsidian_cost) {
-            stack.push_back(build_geode_robot(advance(state), bp));
+        long const ore_time = time_until_ore(state, bp.ore_robot_ore_cost);
+        if (state.time_left > ore_time && state.ore_robots < bp.max_ore_cost()) {
+            stack.push_back(build_ore_robot(advance(state, ore_time + 1), bp));
         }
 
-        stack.push_back(advance(state));
-
+        stack.push_back(advance(state, state.time_left));
     }
 
     return most_geodes;
